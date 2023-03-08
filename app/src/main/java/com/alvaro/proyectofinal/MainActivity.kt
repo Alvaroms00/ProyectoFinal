1ce8a74de350d991f7dc600ef84c313cd0cab823
Alvaroms00 <alvaro030400@gmail.com>
2023-03-08 11:09:39 +0100

    Version 0.0.1.2

--- app/src/main/java/com/alvaro/proyectofinal/MainActivity.kt
+++ app/src/main/java/com/alvaro/proyectofinal/MainActivity.kt
@@ -2,10 +2,25 @@ package com.alvaro.proyectofinal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.alvaro.proyectofinal.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        val screenSplash = installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(ActivityMainBinding.inflate(layoutInflater).also { binding = it }.root)
+
+        CoroutineScope(Dispatchers.IO).launch{
+            delay(1000)
+            screenSplash.setKeepOnScreenCondition{ false }
+        }
     }
+
+
 }
