d0e8b6a27be15b33102632b8999c3c69d1224bf4
Alvaroms00 <alvaro030400@gmail.com>
2023-03-10 11:37:00 +0100

    Version 0.0.2.1

---
+++ app/src/main/java/com/alvaro/proyectofinal/InicioFragment.kt
@@ -0,0 +1,48 @@
+package com.alvaro.proyectofinal
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.alvaro.proyectofinal.databinding.FragmentInicioBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [InicioFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class InicioFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+   lateinit var binding: FragmentInicioBinding
+   lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        binding = FragmentInicioBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = findNavController()
+
+        binding.btnContinuar.setOnClickListener {
+            navController.navigate(R.id.action_inicioFragment_to_presentacionFragment)
+        }
+    }
+
+}
--- app/src/main/java/com/alvaro/proyectofinal/MainActivity.kt
+++ app/src/main/java/com/alvaro/proyectofinal/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.alvaro.proyectofinal
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.alvaro.proyectofinal.databinding.ActivityMainBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -11,14 +14,25 @@ import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         val screenSplash = installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(ActivityMainBinding.inflate(layoutInflater).also { binding = it }.root)
 
         CoroutineScope(Dispatchers.IO).launch{
             delay(1000)
             screenSplash.setKeepOnScreenCondition{ false }
+        }
+
+        val usuario = getSharedPreferences("usuario", Context.MODE_PRIVATE)
+        var usuarioGuardado = usuario.getString("usuario", "")
+
+        if (usuarioGuardado != ""){
+
+        }else{
+            setContentView(ActivityMainBinding.inflate(layoutInflater).also { binding = it }.root)
+            val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+            navController = navHostFragment.navController
         }
     }
 
