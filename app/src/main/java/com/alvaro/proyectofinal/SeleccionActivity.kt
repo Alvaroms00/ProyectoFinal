b599767e21207df5edf297c14807fb888f647086
Alvaro Moreno <alvaro030400@gmail.com>
2023-04-25 18:18:02 +0200

    Version 0.0.5.1 25/04/23 Creación del NavigationView y menu Hamburguesa

--- app/src/main/java/com/alvaro/proyectofinal/SeleccionActivity.kt
+++ app/src/main/java/com/alvaro/proyectofinal/SeleccionActivity.kt
@@ -2,10 +2,30 @@ package com.alvaro.proyectofinal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.alvaro.proyectofinal.databinding.ActivitySeleccionBinding
 
 class SeleccionActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySeleccionBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_seleccion)
+        setContentView(ActivitySeleccionBinding.inflate(layoutInflater).also { binding = it }.root)
+        setSupportActionBar(binding.toolbar)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        appBarConfiguration = AppBarConfiguration(setOf(
+
+            ),
+            binding.drawerLayout
+        )
+
+        NavigationUI.setupWithNavController(binding.navView, navController)
+        NavigationUI.setupWithNavController(binding.toolbar,navController,appBarConfiguration)
     }
 }
