9b865c06e4dc351ffb2ae31044bc2debd68254c3
Alvaro Moreno <alvaro030400@gmail.com>
2023-04-25 18:02:06 +0200

    Version 0.0.4 25/04/23 Cambios en el OnBoarding

--- app/src/main/java/com/alvaro/proyectofinal/InicioFragment.kt
+++ app/src/main/java/com/alvaro/proyectofinal/InicioFragment.kt
@@ -12,18 +12,12 @@ import com.alvaro.proyectofinal.databinding.FragmentInicioBinding
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 
-/**
- * A simple [Fragment] subclass.
- * Use the [InicioFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class InicioFragment : Fragment() {
     // TODO: Rename and change types of parameters
    lateinit var binding: FragmentInicioBinding
    lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
--- app/src/main/java/com/alvaro/proyectofinal/PresentacionFragment.kt
+++ app/src/main/java/com/alvaro/proyectofinal/PresentacionFragment.kt
@@ -5,28 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.alvaro.proyectofinal.databinding.FragmentPresentacionBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [PresentacionFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PresentacionFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    lateinit var binding: FragmentPresentacionBinding
+    lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -34,26 +26,16 @@ class PresentacionFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_presentacion, container, false)
+        binding = FragmentPresentacionBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PresentacionFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PresentacionFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = findNavController()
+
+        binding.btnContinuar.setOnClickListener{
+            navController.navigate(R.id.action_presentacionFragment_to_puntuacionFragment)
+        }
     }
 }
