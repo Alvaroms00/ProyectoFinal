09070057720ecb940e658979bf40b0a899af1c2f
Alvaro Moreno <alvaro030400@gmail.com>
2023-04-29 16:52:32 +0200

    0.0.7.5 29/04/23

--- app/src/main/java/com/alvaro/proyectofinal/RegistroFragment.kt
+++ app/src/main/java/com/alvaro/proyectofinal/RegistroFragment.kt
@@ -7,7 +7,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.navigation.NavController
+import com.alvaro.proyectofinal.BaseDeDatos.Database
 import com.alvaro.proyectofinal.databinding.FragmentRegistroBinding
 
 // TODO: Rename parameter arguments, choose names that match
@@ -20,6 +22,7 @@ class RegistroFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
@@ -34,9 +37,19 @@ class RegistroFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
         binding.btnContinuar.setOnClickListener{
             var usuario = this.activity?.getSharedPreferences(getString(R.string.usuario), Context.MODE_PRIVATE)
             usuario?.edit()?.putString("usuario",binding.introUser.text.toString())?.apply()
+
+            val connection = Database.getConnection()
+            val statement = connection?.prepareStatement("INSERT INTO usuarios (nombre, usuario, pass) VALUES (?,?,?)")
+            statement?.setString(1,binding.introNombre.text.toString())
+            statement?.setString(2,binding.introUser.text.toString())
+            statement?.setString(3,binding.introPass.text.toString())
+            statement?.executeUpdate()
+            connection?.close()
+
 
             val intent = Intent(activity,MainActivity::class.java)
             startActivity(intent)
