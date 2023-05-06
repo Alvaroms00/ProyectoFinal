b57ca095bc101ac92a47a96e8dc7962c55eccbeb
Alvaro Moreno <alvaro030400@gmail.com>
2023-05-06 11:46:12 +0200

    0.1.14.2 04/05/23 Modificaciones en las ventanas

--- app/src/main/java/com/alvaro/proyectofinal/OnBoarding/RegistroFragment.kt
+++ app/src/main/java/com/alvaro/proyectofinal/OnBoarding/RegistroFragment.kt
@@ -1,14 +1,17 @@
 package com.alvaro.proyectofinal.OnBoarding
 
 import android.app.AlertDialog
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.alvaro.proyectofinal.MainActivity
 import com.alvaro.proyectofinal.ProviderType
+import com.alvaro.proyectofinal.R
 import com.alvaro.proyectofinal.databinding.FragmentRegistroBinding
 import com.google.firebase.auth.FirebaseAuth
 
@@ -40,6 +43,12 @@ class RegistroFragment : Fragment() {
                 FirebaseAuth.getInstance().createUserWithEmailAndPassword(binding.introEmail.text.toString(),binding.introPass.text.toString()).addOnCompleteListener {
                     if (it.isSuccessful){
                         mostrarMenu(it.result?.user?.email ?:"", ProviderType.BASIC)
+
+                        val usuario = this.activity?.getSharedPreferences(getString(R.string.usuario), Context.MODE_PRIVATE)
+                        usuario?.edit()?.putString("usuario", binding.introNombre.text.toString())?.apply()
+
+                        Toast.makeText(activity,"Registro Completado",Toast.LENGTH_SHORT).show()
+                        onDestroy()
                     }else{
                         alerta()
                     }
