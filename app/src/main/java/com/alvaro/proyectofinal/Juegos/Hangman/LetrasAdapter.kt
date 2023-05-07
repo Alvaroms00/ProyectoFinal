ab15e456de44bc1a73aa2728859462be45ac7160
Alvaroms00 <alvaro030400@gmail.com>
2023-05-07 18:15:29 +0200

    0.1.15.2 Cambios en el juego del ahorcado

--- app/src/main/java/com/alvaro/proyectofinal/Juegos/Hangman/LetrasAdapter.kt
+++ app/src/main/java/com/alvaro/proyectofinal/Juegos/Hangman/LetrasAdapter.kt
@@ -1,52 +1,39 @@
-package com.alvaro.proyectofinal.Juegos.Hangman;
+package com.alvaro.proyectofinal.Juegos.Hangman
 
-import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.Button;
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.Button
+import com.alvaro.proyectofinal.R
 
-import com.alvaro.proyectofinal.R;
+class LetrasAdapter(context: Context?) : BaseAdapter() {
+    private val letras: Array<String?> = arrayOfNulls(26)
+    private val letrasLayout: LayoutInflater
 
-public class LetrasAdapter extends BaseAdapter {
-
-    private String[] letras;
-    private LayoutInflater letrasLayout;
-
-    public LetrasAdapter(Context context){
-       letras = new String[26];
-       for (int i = 0; i < letras.length; i++){
-           letras[i] = "" + (char)(i + 'A');
-       }
-       letrasLayout = LayoutInflater.from(context);
-
-    }
-
-    @Override
-    public int getCount() {
-        return letras.length;
-    }
-
-    @Override
-    public Object getItem(int i) {
-        return null;
-    }
-
-    @Override
-    public long getItemId(int i) {
-        return 0;
-    }
-
-    @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        Button btnLetra;
-        if (view == null){
-            btnLetra = (Button) letrasLayout.inflate(R.layout.letras,viewGroup,false);
-        }else {
-            btnLetra = (Button)view;
+    init {
+        for (i in letras.indices) {
+            letras[i] = "" + (i + 'A'.code).toChar()
         }
-        btnLetra.setText(letras[i]);
-        return btnLetra;
+        letrasLayout = LayoutInflater.from(context)
+    }
+
+    override fun getCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItem(i: Int): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemId(i: Int): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun getView(i: Int, view: View, viewGroup: ViewGroup): View {
+        val btnLetra: Button = view as Button
+        btnLetra.text = letras[i]
+        return btnLetra
     }
 }
