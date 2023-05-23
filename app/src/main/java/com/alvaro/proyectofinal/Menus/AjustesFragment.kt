f5d6ac048bf1bd9571b7c7716644798ea461aa81
Alvaroms00 <alvaro030400@gmail.com>
2023-05-24 00:19:55 +0200

    0.3.29.2 23/05/23 Cambios en el onBoarding

--- app/src/main/java/com/alvaro/proyectofinal/Menus/AjustesFragment.kt
+++ app/src/main/java/com/alvaro/proyectofinal/Menus/AjustesFragment.kt
@@ -1,20 +1,14 @@
 package com.alvaro.proyectofinal.Menus
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceFragmentCompat
 import com.alvaro.proyectofinal.R
 
-class AjustesFragment : Fragment() {
+class AjustesFragment : PreferenceFragmentCompat() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ajustes, container, false)
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferencias, rootKey)
     }
+
 
 }
